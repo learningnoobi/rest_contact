94c00a838005209c419f99efd47483ec7c2747b1
learningnoobi <64244098+learningnoobi@users.noreply.github.com>
2020-12-20 13:42:19 +0545

    sensitive keys

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^g!(d-z7j_f8a-+so%$vtk-lv9e%^8)2%9^&7-sf1=$rc_%!(t'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
